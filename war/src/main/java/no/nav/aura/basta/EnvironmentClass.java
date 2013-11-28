18bb6cf20d265d6e0799e38af9fc0c5a2cabe6eb
Thor Åge Eldby <Thor.Age.Eldby@nav.no>
2013-11-28 15:36:51 +0100

    AURA-269: Clean up the mess that is enum-conversions. Settle on u, t, p, q as default

--- war/src/main/java/no/nav/aura/basta/EnvironmentClass.java
+++
@@ -1,9 +0,0 @@
-package no.nav.aura.basta;
-
-public enum EnvironmentClass {
-    u, t, q, p;
-
-    public static EnvironmentClass from(Enum<?> e) {
-        return valueOf(e.name().substring(0, 1));
-    }
-}
