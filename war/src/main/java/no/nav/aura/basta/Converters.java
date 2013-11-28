cd68b055c800bea6b93bffa6386aa790906fe904
Thor Åge Eldby <Thor.Age.Eldby@nav.no>
2013-11-28 16:36:03 +0100

    AURA-269: Use domain logic from Fasit client api

--- war/src/main/java/no/nav/aura/basta/Converters.java
+++ war/src/main/java/no/nav/aura/basta/Converters.java
@@ -1,24 +1,40 @@
 package no.nav.aura.basta;
 
-import static java.util.Arrays.asList;
+import java.util.Arrays;
+
 import no.nav.aura.basta.persistence.EnvironmentClass;
 import no.nav.aura.basta.persistence.Zone;
+import no.nav.aura.basta.util.SerializablePredicate;
 import no.nav.aura.basta.vmware.orchestrator.request.ProvisionRequest.envClass;
+import no.nav.aura.envconfig.client.DomainDO;
+import no.nav.aura.envconfig.client.DomainDO.EnvClass;
 
-import com.google.common.base.Functions;
+import com.google.common.collect.FluentIterable;
 import com.google.common.collect.ImmutableMap;
-import com.google.common.collect.Maps;
 
 public class Converters {
 
-    private static ImmutableMap<EnvironmentClass, ImmutableMap<Zone, String>> environmentClassMap = ImmutableMap.of(
-            EnvironmentClass.u, Maps.toMap(asList(Zone.values()), Functions.constant("devillo.no")),
-            EnvironmentClass.t, ImmutableMap.of(Zone.fss, "test.local", Zone.sbs, "oera-t.local"),
-            EnvironmentClass.q, ImmutableMap.of(Zone.fss, "preprod.local", Zone.sbs, "oera-q.local"),
-            EnvironmentClass.p, ImmutableMap.of(Zone.fss, "adeo.no", Zone.sbs, "oera.no"));
+    @SuppressWarnings("serial")
+    public static String domainFrom(final EnvironmentClass environmentClass, final Zone zone) {
+        return FluentIterable.from(Arrays.asList(DomainDO.values())).filter(new SerializablePredicate<DomainDO>() {
+            public boolean test(DomainDO domain) {
+                try {
+                    return domain.getClass().getField(domain.name()).getAnnotation(Deprecated.class) == null
+                            && domain.isInZone(fasitZoneFromLocal(zone))
+                            && domain.getEnvironmentClass() == fasitEnvironmentClassFromLocal(environmentClass);
+                } catch (NoSuchFieldException | SecurityException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }).first().get().getFqn();
+    }
 
-    public static String domainFrom(EnvironmentClass environmentClass, Zone zone) {
-        return environmentClassMap.get(environmentClass).get(zone);
+    public static EnvClass fasitEnvironmentClassFromLocal(EnvironmentClass environmentClass) {
+        return EnvClass.valueOf(environmentClass.name());
+    }
+
+    public static no.nav.aura.envconfig.client.DomainDO.Zone fasitZoneFromLocal(Zone zone) {
+        return no.nav.aura.envconfig.client.DomainDO.Zone.valueOf(zone.name().toUpperCase());
     }
 
     private static ImmutableMap<EnvironmentClass, envClass> orchestratorEnvironmentClassFromLocalMap = ImmutableMap.of(
--- war/src/test/java/no/nav/aura/basta/ConvertersTest.java
+++ war/src/test/java/no/nav/aura/basta/ConvertersTest.java
@@ -5,24 +5,27 @@ import static org.junit.Assert.assertThat;
 
 import java.util.Set;
 
-import no.nav.aura.basta.Converters;
 import no.nav.aura.basta.persistence.EnvironmentClass;
 import no.nav.aura.basta.persistence.Zone;
+import no.nav.aura.basta.util.SerializableFunction;
 import no.nav.aura.basta.vmware.orchestrator.request.ProvisionRequest.envClass;
+import no.nav.aura.envconfig.client.DomainDO.EnvClass;
 
 import org.junit.Test;
 
+import com.google.common.base.Function;
 import com.google.common.collect.Sets;
 
+@SuppressWarnings("serial")
 public class ConvertersTest {
 
     @Test
     public void orchestratorEnvironmentClassFromLocal() {
-        Set<envClass> list = Sets.newHashSet();
-        for (EnvironmentClass environmentClass : EnvironmentClass.values()) {
-            list.add(Converters.orchestratorEnvironmentClassFromLocal(environmentClass));
-        }
-        assertThat(list.size(), equalTo(4));
+        checkEnumConversion(EnvironmentClass.values(), new SerializableFunction<EnvironmentClass, envClass>() {
+            public envClass process(EnvironmentClass input) {
+                return Converters.orchestratorEnvironmentClassFromLocal(input);
+            }
+        });
     }
 
     @Test
@@ -30,5 +33,31 @@ public class ConvertersTest {
         assertThat(Converters.domainFrom(EnvironmentClass.u, Zone.fss), equalTo("devillo.no"));
         assertThat(Converters.domainFrom(EnvironmentClass.p, Zone.fss), equalTo("adeo.no"));
         assertThat(Converters.domainFrom(EnvironmentClass.p, Zone.sbs), equalTo("oera.no"));
+    }
+
+    @Test
+    public void fasitZoneFromLocal() {
+        checkEnumConversion(Zone.values(), new SerializableFunction<Zone, no.nav.aura.envconfig.client.DomainDO.Zone>() {
+            public no.nav.aura.envconfig.client.DomainDO.Zone process(Zone input) {
+                return Converters.fasitZoneFromLocal(input);
+            }
+        });
+    }
+
+    @Test
+    public void fasitEnvironmentClassFromLocal() {
+        checkEnumConversion(EnvironmentClass.values(), new SerializableFunction<EnvironmentClass, EnvClass>() {
+            public EnvClass process(EnvironmentClass input) {
+                return Converters.fasitEnvironmentClassFromLocal(input);
+            }
+        });
+    }
+
+    private <T, F> void checkEnumConversion(F[] values, Function<F, T> f) {
+        Set<T> set = Sets.newHashSet();
+        for (F environmentClass : values) {
+            set.add(f.apply(environmentClass));
+        }
+        assertThat(set.size(), equalTo(values.length));
     }
 }
