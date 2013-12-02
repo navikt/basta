555bedd38bd571c7cc727f49ba5fd311298d5a07
Thor Åge Eldby <Thor.Age.Eldby@nav.no>
2013-12-02 13:26:00 +0100

    AURA-246: Added multisite test

--- war/src/test/java/no/nav/aura/basta/order/OrderV2FactoryTest.java
+++ war/src/test/java/no/nav/aura/basta/order/OrderV2FactoryTest.java
@@ -1,5 +1,10 @@
 package no.nav.aura.basta.order;
 
+import static org.hamcrest.Matchers.containsInAnyOrder;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasProperty;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
 import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
@@ -23,6 +28,7 @@ import no.nav.aura.basta.util.Effect;
 import no.nav.aura.basta.util.SpringRunAs;
 import no.nav.aura.basta.vmware.XmlUtils;
 import no.nav.aura.basta.vmware.orchestrator.request.ProvisionRequest;
+import no.nav.aura.basta.vmware.orchestrator.request.VApp.Site;
 import no.nav.aura.envconfig.client.DomainDO;
 import no.nav.aura.envconfig.client.FasitRestClient;
 import no.nav.aura.envconfig.client.ResourceTypeDO;
@@ -41,6 +47,8 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.xml.sax.SAXException;
+
+import com.google.common.collect.Lists;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = { SpringUnitTestConfig.class })
@@ -71,17 +79,52 @@ public class OrderV2FactoryTest extends XMLTestCase {
             public void perform() {
                 try {
                     FasitRestClient fasitRestClient = createFasitMock();
-                    ProvisionRequest order = new OrderV2Factory(settings, "admin", new URI("http://thisisbasta/orders/vm"), new URI("http://thisisbasta/orders/results"), fasitRestClient).createOrder();
+                    ProvisionRequest order = new OrderV2Factory(settings, "admin", createURI("http://thisisbasta/orders/vm"), createURI("http://thisisbasta/orders/results"), fasitRestClient).createOrder();
                     String xml = XmlUtils.prettyFormat(XmlUtils.generateXml(order), 2);
                     // System.out.println("### xml: " + xml);
                     Diff diff = new Diff(new InputStreamReader(getClass().getResourceAsStream(expectXml)), new StringReader(xml));
                     diff.overrideElementQualifier(new ElementNameAndAttributeQualifier());
                     assertXMLEqual(diff, true);
-                } catch (JAXBException | SAXException | IOException | URISyntaxException e) {
+                } catch (JAXBException | SAXException | IOException e) {
                     throw new RuntimeException(e);
                 }
             }
         });
+    }
+
+    @SuppressWarnings({ "unchecked", "serial" })
+    @Test
+    public void createMultisite() {
+        SpringRunAs.runAs(authenticationManager, "admin", "admin", new Effect() {
+            public void perform() {
+                for (EnvironmentClass environmentClass : EnvironmentClass.values()) {
+                    for (Boolean multisite : Lists.newArrayList(true, false)) {
+                        SettingsDO settings = createRequestJbossSettings();
+                        settings.setMultisite(multisite);
+                        settings.setEnvironmentClass(environmentClass);
+                        FasitRestClient fasitRestClient = createFasitMock();
+                        ProvisionRequest order = new OrderV2Factory(settings, "admin", createURI("http://thisisbasta/orders/vm"), createURI("http://thisisbasta/orders/results"), fasitRestClient).createOrder();
+                        if (environmentClass == EnvironmentClass.p || (multisite && environmentClass == EnvironmentClass.q)) {
+                            assertThat(order.getvApps().size(), is(2));
+                            assertThat(order.getvApps(), containsInAnyOrder(
+                                    hasProperty("site", equalTo(Site.u89)),
+                                    hasProperty("site", equalTo(Site.so8))));
+                        } else {
+                            assertThat(order.getvApps().size(), is(1));
+                            assertThat(order.getvApps().get(0).getSite(), equalTo(Site.so8));
+                        }
+                    }
+                }
+            }
+        });
+    }
+
+    private URI createURI(String uri) {
+        try {
+            return new URI(uri);
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     private FasitRestClient createFasitMock() {
