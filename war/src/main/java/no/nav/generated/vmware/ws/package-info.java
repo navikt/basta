1ecd718450c1e878650a0ba4302f544c6d0c2ef6
Thor Åge Eldby <Thor.Age.Eldby@nav.no>
2013-12-05 16:13:21 +0100

    AURA-249: Updated wsdl, deleted all checked in generated files and started generating on wsdl

--- war/src/main/java/no/nav/generated/vmware/ws/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://webservice.vso.dunes.ch", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package no.nav.generated.vmware.ws;
