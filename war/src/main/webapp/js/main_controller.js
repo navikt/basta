62941cfd3b70a87f05ee6cec334af3b6d7ded861
Thor Åge Eldby <Thor.Age.Eldby@nav.no>
2013-12-11 16:36:35 +0100

    AURA-304: Added error handlers as well (while I'm at it)

--- war/src/main/webapp/js/main_controller.js
+++ war/src/main/webapp/js/main_controller.js
@@ -2,11 +2,31 @@
 
 angular.module('skyBestApp.main_controller', [])
   .controller('mainController', ['$scope', '$http', '$templateCache', '$location', '$resource', function($scope, $http, $templateCache, $location, $resource) {
+    // TODO duplicated from order_form_controller.js
+    function getField(object, fields) {
+      if (object == null || fields.length == 0) 
+        return object;
+      else {
+        var name = fields[0];
+        fields.shift();
+        return getField(object[name], fields);
+      }
+    }
+    function errorHandler(data, status, headers, config) {
+      var message = 'Feil oppstått! Http-kode ' + status;
+      var detailedMessage = getField(data, ['html', 'head', 'title']);
+      if (detailedMessage) {
+        message += ' melding "' + detailedMessage + '"';
+      }
+      $scope.userForm.error = message;
+    }
     function retrieveUser() {
       $resource('/rest/users/:identifier').get({identifier: "current"}, function(data) {
         $scope.currentUser = data;
-      });
+      }, errorHandler);
     }
+
+    $scope.userForm = {};
 
     $scope.clearCache = function() {
         $templateCache.removeAll();
@@ -27,10 +47,10 @@ angular.module('skyBestApp.main_controller', [])
         } else {
           $scope.userForm.error = 'Innlogging feilet';
         }
-      });
+      }).error(errorHandler);
     };
     $scope.logout = function() {
-      $http.get('logout');
+      $http.get('logout').error(errorHandler);
       $scope.$broadcast("UserChanged");
     };
 
