8656e5052ee37accb2391853dffa3d48d1b70bce
Thor Aage Eldby <thor.age.eldby@nav.no>
2014-02-17 13:08:28 +0100

    Started a spec on node list controller

---
+++ war/src/test/js/unit/spec_node_list_controller.js
@@ -0,0 +1,37 @@
+'use strict';
+
+describe('node_list_controller', function() {
+    //load the controllers module
+    beforeEach(module('skyBestApp'));
+
+    var $scope, $httpBackend;
+
+    beforeEach(inject(function(_$httpBackend_, $rootScope, $location, $controller) {
+      $httpBackend = _$httpBackend_;
+      $scope = $rootScope.$new();
+      $controller('nodeListController', {
+          '$scope': $scope
+      });
+      $httpBackend.whenGET(/rest\/nodes.*/).respond(200, [{id: 1}, {id: 2}, {id: 3}, {id: 4}, {id: 5}], {'content-type' : 'application/text'} );
+      $httpBackend.whenGET('/rest/users/current').respond(
+          {username:'the username',
+              authenticated:true,
+              environmentClasses:[]
+          });
+    }));
+
+    it("should retrieve nodes on startup", function() {
+        $httpBackend.flush();
+        expect(_.isArray($scope.selectedNodes)).toBe(true);
+        expect(_.isEmpty($scope.selectedNodes)).toBe(true);
+        expect($scope.nodes.length).toBe(5);
+    });
+    
+    it("should enable select node", function() {
+      $httpBackend.flush();
+      expect($scope.selectedNodes.length).toBe(0);
+      $scope.setSelectedNode({id: 3});
+      expect($scope.selectedNodes.length).toBe(1);
+  });
+    
+});
