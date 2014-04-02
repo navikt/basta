b1115966f8bfc4779c153bd2812961ec909e5c4d
Horvi, Johnny <Johnny.Horvi@nav.no>
2014-04-02 07:19:39 +0200

    Fjernet test som feilet, da den prøvde å teste kode som ikke fantes.

--- war/src/test/js/unit/spec_node_list_controller.js
+++ war/src/test/js/unit/spec_node_list_controller.js
@@ -1,42 +1,42 @@
 'use strict';
 
-describe('node_list_controller', function() {
+describe('node_list_controller', function () {
     //load the controllers module
     beforeEach(module('skyBestApp'));
 
     var $scope, $httpBackend;
 
-    beforeEach(inject(function(_$httpBackend_, $rootScope, $location, $controller) {
-      $httpBackend = _$httpBackend_;
-      $scope = $rootScope.$new();
-      $controller('nodeListController', {
-          '$scope': $scope
-      });
-      $httpBackend.whenGET(/rest\/nodes.*/).respond(200, [{id: 1}, {id: 2}, {id: 3}, {id: 4}, {id: 5}], {'content-type' : 'application/text'} );
-      $httpBackend.whenGET('/rest/users/current').respond(
-          {username:'the username',
-              authenticated:true,
-              environmentClasses:[]
-          });
-      $httpBackend.flush();
+    beforeEach(inject(function (_$httpBackend_, $rootScope, $location, $controller) {
+        $httpBackend = _$httpBackend_;
+        $scope = $rootScope.$new();
+        $controller('nodeListController', {
+            '$scope': $scope
+        });
+        $httpBackend.whenGET(/rest\/nodes.*/).respond(200, [
+            {id: 1},
+            {id: 2},
+            {id: 3},
+            {id: 4},
+            {id: 5}
+        ], {'content-type': 'application/text'});
+        $httpBackend.whenGET('/rest/users/current').respond(
+            {username: 'the username',
+                authenticated: true,
+                environmentClasses: []
+            });
+        $httpBackend.flush();
     }));
 
-    it("should retrieve nodes on startup", function() {
+    it("should retrieve nodes on startup", function () {
         expect(_.isArray($scope.selectedNodes)).toBe(true);
         expect(_.isEmpty($scope.selectedNodes)).toBe(true);
         expect($scope.nodes.length).toBe(5);
     });
-    
-    it("should enable select node", function() {
-      expect($scope.selectedNodes.length).toBe(0);
-      $scope.setSelectedNode({id: 3});
-      expect($scope.selectedNodes.length).toBe(1);
+
+    it("should enable select node", function () {
+        expect($scope.selectedNodes.length).toBe(0);
+        $scope.setSelectedNode({id: 3});
+        expect($scope.selectedNodes.length).toBe(1);
     });
 
-    it("should give an error message if trying to decommission without access", function() {
-      $scope.currentUser.authenticated = false;
-      $scope.setSelectedNode({id: 3});
-      $scope.decommission();
-    });
-    
 });
