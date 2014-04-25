6d7f30a16d8f7157afaeb2b7d5ed1651019d323d
Jorgensen, Truls <truls.jorgensen@nav.no>
2014-04-25 10:50:40 +0200

    AURA-689 styling

--- war/src/main/webapp/js/directives/angular-toggle-switch.js
+++ war/src/main/webapp/js/directives/angular-toggle-switch.js
@@ -29,8 +29,8 @@ angular.module('toggle-switch', ['ng'])
             };
         },
         compile: function(element, attrs) {
-            if (!attrs.onLabel) { attrs.onLabel = ''; }
-            if (!attrs.offLabel) { attrs.offLabel = ''; }
+            if (!attrs.onLabel) { attrs.onLabel = '.'; }
+            if (!attrs.offLabel) { attrs.offLabel = '.'; }
             if (!attrs.knobLabel) { attrs.knobLabel = '\u00a0'; }
             if (!attrs.disabled) { attrs.disabled = false; }
             if (!attrs.colorLeft){
