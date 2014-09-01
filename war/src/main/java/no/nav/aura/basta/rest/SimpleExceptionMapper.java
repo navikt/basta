5fd70c23dddfdc0a6006e537e33aa65582d4a33e
Hans-Petter Vadseth <v137023@nav.no>
2014-09-01 15:49:15 +0200

    Exception mapper and logging

---
+++ war/src/main/java/no/nav/aura/basta/rest/SimpleExceptionMapper.java
@@ -0,0 +1,42 @@
+package no.nav.aura.basta.rest;
+
+import static javax.ws.rs.core.MediaType.TEXT_PLAIN_TYPE;
+
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+
+import org.jboss.resteasy.spi.Failure;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Provider
+@Component
+public class SimpleExceptionMapper implements ExceptionMapper<RuntimeException> {
+
+    private static final Logger logger = LoggerFactory.getLogger(SimpleExceptionMapper.class);
+
+    @Override
+    public Response toResponse(RuntimeException e) {
+        int status;
+        String message;
+        if (e instanceof Failure) {
+            // Errors from RestEasy
+            Failure f = (Failure) e;
+            status = f.getErrorCode();
+            message = f.getMessage();
+            logger.warn("Rest returned code: {} reason: {}", status, message);
+
+        } else if (e instanceof IllegalArgumentException) {
+            status = Response.Status.BAD_REQUEST.getStatusCode();
+            message = e.getMessage();
+            logger.warn("Bad request", e);
+        } else {
+            status = Response.Status.INTERNAL_SERVER_ERROR.getStatusCode();
+            message = e.getMessage();
+            logger.error("Internal error", e);
+        }
+        return Response.status(status).entity(message).type(TEXT_PLAIN_TYPE).build();
+    }
+}
