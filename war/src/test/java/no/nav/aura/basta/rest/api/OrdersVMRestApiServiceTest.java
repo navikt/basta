d9610dd9f7a388e5ed6d977d4555cfc0cee60d3b
Hans-Petter Vadseth <v137023@nav.no>
2015-03-05 14:47:54 +0100

    Downgrade httpclient dependency in rest-assured

--- war/src/test/java/no/nav/aura/basta/rest/api/OrdersVMRestApiServiceTest.java
+++ war/src/test/java/no/nav/aura/basta/rest/api/OrdersVMRestApiServiceTest.java
@@ -17,44 +17,42 @@ public class OrdersVMRestApiServiceTest extends RestTest {
     public void checkDecommisionCallback() {
         Order order = repository.save(Order.newDecommissionOrder("host1.devillo.no"));
         given()
-                .log().ifValidationFails()
                 .auth().basic("prodadmin", "prodadmin")
                 .body("<vm><hostName>host1.devillo.no</hostName></vm>")
                 .contentType(ContentType.XML)
+                .expect()
+                .statusCode(204)
+                .log().ifError()
                 .when()
-                .put("/rest/api/orders/vm/{orderId}/decommission", order.getId())
-                .then()
-                .log().ifValidationFails()
-                .statusCode(204);
+                .put("/rest/api/orders/vm/{orderId}/decommission", order.getId());
     }
 
     @Test
     public void checkStartCallback() {
         Order order = repository.save(Order.newStartOrder("host1.devillo.no"));
         given()
-                .log().ifValidationFails()
                 .auth().basic("prodadmin", "prodadmin")
                 .body("<vm><hostName>host1.devillo.no</hostName></vm>")
                 .contentType(ContentType.XML)
+                .expect()
+                .log().ifError()
+                .statusCode(204)
                 .when()
-                .put("/rest/api/orders/vm/{orderId}/start", order.getId())
-                .then()
-                .log().ifValidationFails()
-                .statusCode(204);
+                .put("/rest/api/orders/vm/{orderId}/start", order.getId());
+
     }
 
     @Test
     public void checkStopCallback() {
         Order order = repository.save(Order.newStopOrder("host1.devillo.no"));
         given()
-                .log().ifValidationFails()
                 .auth().basic("prodadmin", "prodadmin")
                 .body("<vm><hostName>host1.devillo.no</hostName></vm>")
                 .contentType(ContentType.XML)
+                .expect()
+                .log().ifError()
+                .statusCode(204)
                 .when()
-                .put("/rest/api/orders/vm/{orderId}/stop", order.getId())
-                .then()
-                .log().ifValidationFails()
-                .statusCode(204);
+                .put("/rest/api/orders/vm/{orderId}/stop", order.getId());
     }
 }
