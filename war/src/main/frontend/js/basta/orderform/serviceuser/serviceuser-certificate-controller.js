a2c1ec005a59c3281d24fe54b058475d86487c61
Hans-Petter Vadseth <v137023@nav.no>
2015-04-30 14:50:59 +0200

    Legger til autentisering på hele sertifikatbestilling siden

--- war/src/main/frontend/js/basta/orderform/serviceuser/serviceuser-certificate-controller.js
+++ war/src/main/frontend/js/basta/orderform/serviceuser/serviceuser-certificate-controller.js
@@ -1,49 +1,59 @@
 'use strict';
 
-module.exports = ['$http', '$location', 'errorService','FasitService',
-		function ( $http, $location, errorService, FasitService ){
-			this.settings={zone:'fss', environmentClass:'u', application:''}
-			
-			 var updateChoices = function (data) {
-				 this.choices = data;
-		     };
-		     
-		    FasitService.applications.then(updateChoices.bind(this))
-			
-			this.isInFasit=false;
-		    var ctrl=this;
-			
-			this.changeEnvironmentClass = function () {
+module.exports = ['$http', '$location','$scope', 'errorService','FasitService', 'User', function ( $http, $location,$scope, errorService, FasitService, User ){
+	
+	  var setAuthenticated = function (auth) {
+          this.authenticated = auth;
+      };
+
+      $scope.$on('UserUpdated', function(){
+          User.authenticated().then(setAuthenticated.bind(this));
+      }.bind(this));
+
+      User.authenticated().then(setAuthenticated.bind(this));
+
+
+		this.settings={zone:'fss', environmentClass:'u', application:''}
+		
+		 var updateChoices = function (data) {
+			 this.choices = data;
+	     };
+	     
+	    FasitService.applications.then(updateChoices.bind(this))
+		
+		this.isInFasit=false;
+	    var ctrl=this;
+		
+		this.changeEnvironmentClass = function () {
+           
+            if (this.settings.environmentClass === 'u') {
+                this.settings.zone = 'fss';
+            }
+         }
+		 
+         this.changeApplication= function(){
+	        	console.log("changed application to "+ this.settings.application)
+	        	checkIfResourceExistInFasit(this.settings);
+	        }
+	        	
                
-                if (this.settings.environmentClass === 'u') {
-                    this.settings.zone = 'fss';
-                }
-	         }
-			 
-	         this.changeApplication= function(){
-		        	console.log("changed application to "+ this.settings.application)
-		        	checkIfResourceExistInFasit(this.settings);
-		        }
-		        	
-	               
-	        function checkIfResourceExistInFasit(settings){
-		        	 $http.get('rest/orders/serviceuser/Certificate/resourceExists',{ params: _.omit(settings)})
-		        	.error(errorService.handleHttpError('Fasit sjekk om sertifikat eksisterer'))
-		        	.success(function(data){
-			            ctrl.isInFasit =  data; 
-			        });
-		         };
-
-	         this.submitOrder= function(){
-	        	 $http.post('rest/orders/serviceuser/certificate',_.omit(this.settings))
-                 	.success(onOrderSuccess)
-                 	.error(errorService.handleHttpError('Bestilling'));
+        function checkIfResourceExistInFasit(settings){
+	        	 $http.get('rest/orders/serviceuser/Certificate/resourceExists',{ params: _.omit(settings)})
+	        	.error(errorService.handleHttpError('Fasit sjekk om sertifikat eksisterer'))
+	        	.success(function(data){
+		            ctrl.isInFasit =  data; 
+		        });
 	         };
-	         
-	         function onOrderSuccess(order) {
-	        	 	console.log("received order " + order);
-	                $location.path('/order_details/' + order.id);
-	            }
 
+         this.submitOrder= function(){
+        	 $http.post('rest/orders/serviceuser/certificate',_.omit(this.settings))
+             	.success(onOrderSuccess)
+             	.error(errorService.handleHttpError('Bestilling'));
+         };
+         
+         function onOrderSuccess(order) {
+        	 	console.log("received order " + order);
+                $location.path('/order_details/' + order.id);
+            }
 
-		}];
+}];
