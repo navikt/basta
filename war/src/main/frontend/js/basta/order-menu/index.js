907c6e7bee60b687bba36a9e39c6a96058f5079a
Hans-Petter Vadseth <v137023@nav.no>
2015-06-16 07:28:04 +0200

    Ny meny

--- war/src/main/frontend/js/basta/order-menu/index.js
+++ war/src/main/frontend/js/basta/order-menu/index.js
@@ -1,2 +1,4 @@
 var basta = require('angular').module('basta');
+basta.controller('orderMenuController', require('./order-menu-controller'));
+//@deprecated
 basta.directive('menuItem', require('./menu-item'));
---
+++ war/src/main/frontend/js/basta/order-menu/order-menu-controller.js
@@ -0,0 +1,28 @@
+'use strict';
+
+var angular = require('angular');
+
+module.exports = ['$location', function ($location) {
+    
+    	function menuItem(header, description,  icon, url, show){
+    	    return {header:header, description:description, icon:icon, url:url}
+    	}
+    
+    
+    	this.menu=[
+    		menuItem("WAS,",'Node','websphere.png','/vm_order?orderType=WAS_NODES'),
+    		menuItem("WAS,",'Deployment Mananger','websphere.png','/vm_order?orderType=WAS_DEPLOYMENT_MANAGER'),
+    		menuItem("BPM,",'Node','websphere.png','vm_order?orderType=BPM_NODES'),
+    		menuItem("BPM,",'Deployment Mananger','websphere.png','/vm_order?orderType=BPM_DEPLOYMENT_MANAGER'),
+    		menuItem("Jboss,",'Application server','jboss.png','/jboss_order'),
+    		menuItem("Red hat,",'Linux','redhat.png','/linux_order'),
+    		menuItem("Credentials,",'for Service user','security.png','/serviceuser_credential_order'),
+    		menuItem("Certificate,",'for Service user','security.png','/serviceuser_certificate_order'),
+    		
+    	];
+
+	this.goTo = function(url) {
+	    $location.url(url);
+	};
+	
+} ];
