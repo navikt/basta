09e2b5bd6bc217cba8a06f3b25732d4359c46fd8
Hans-Petter Vadseth <v137023@nav.no>
2015-06-29 11:04:18 +0200

    Fjerner gammel was bestilling

--- war/src/main/frontend/js/basta/services/basta_backend_service.js
+++ war/src/main/frontend/js/basta/services/basta_backend_service.js
@@ -12,21 +12,6 @@ module.exports = ['$http', '$location', '$q', 'errorService', function( $http, $
         return result;
     }
 
-
-    this.serverSizes = function(){
-        return  $http({method: 'GET', url: 'rest/vm/choices'})
-            .error(errorService.handleHttpError('Valginformasjon'))
-            .then(function onSuccess(response) {
-                var serverSizes = [
-                    { key : 's', name: 'Standard'},
-                    { key : 'm', name: 'Medium'},
-                    { key : 'l', name: 'Stor'}
-                ];
-                _(serverSizes).each(function (serverSize) {_.extend(serverSize, response.data.serverSizes[serverSize.key]);});
-                return serverSizes;
-            });
-    };
-
     this.submitOrderWithUrl = function(url, data){
     	console.log("posting order to ", url)
         $http.post(url, flatMap(data, {}))
@@ -34,31 +19,14 @@ module.exports = ['$http', '$location', '$q', 'errorService', function( $http, $
             .error(errorService.handleHttpError('Ordreinnsending'));
     };
 
-    this.submitOrder = function(data){
-    	this.submitOrderWithUrl('rest/vm/orders', data);
-    };
 
-    this.submitEditedOrder = function(orderid, data){
-        $http.put('rest/vm/orders/' + orderid, data, {headers: {'Content-type': 'text/plain', 'Accept': 'application/json'}})
-            .success(onOrderSuccess)
-            .error(errorService.handleHttpError('Ordreinnsending'));
-    }
-
-    this.editOrder = function(data){
-        return $http.post('rest/vm/orders?prepare=true', flatMap(data,{}))
-            .error(errorService.handleHttpError('Ordreinnsending'));
-    };
 
     function onOrderSuccess(order) {
         $location.path('/order_details/' + order.id)
     }
 
     return {
-        serverSizes : this.serverSizes(),
-        submitOrder: this.submitOrder,
         submitOrderWithUrl: this.submitOrderWithUrl,
-        submitEditedOrder: this.submitEditedOrder,
-        editOrder: this.editOrder
     };
 
 
