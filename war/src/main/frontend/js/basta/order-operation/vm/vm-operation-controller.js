f9c0e5a22f17f591be1ae3b9bc7b52517b6525d1
Hans-Petter Vadseth <v137023@nav.no>
2015-12-15 10:20:34 +0100

    Lookup mot fasit

--- war/src/main/frontend/js/basta/order-operation/vm/vm-operation-controller.js
+++ war/src/main/frontend/js/basta/order-operation/vm/vm-operation-controller.js
@@ -2,17 +2,68 @@
 
 var angular = require('angular');
 
-module.exports = [ '$routeParams', 'BastaService', function($routeParams, BastaService) {
+module.exports = [ '$routeParams', "$http", 'BastaService', 'User', function($routeParams, $http, BastaService, User) {
 
-    this.data = {
-	environmentClass : 'u',
-	zone : 'fss',
-	hostname: $routeParams.hostname,
-    }
+	this.choices = [];
 
-    this.submitOrder = function() {
-	console.log("creating new order", this.data);
-	BastaService.submitOrderWithUrl('rest/vm/operations', this.data);
-    };
+	this.data = {
+		host : {}
+	}
+	this.inSuperUserMode = false;
+	
+	var vm = this;
+	
+	init($routeParams.hostname);
 
+	// Initial setup
+	function init(hostname) {
+		if (hostname) {
+			vm.data.host = {
+				hostname : hostname
+			};
+			var params = {
+				hostname : hostname
+			};
+			return $http.get('http://sera.adeo.no/api/v1/servers', {
+				params : params
+			}).then(function(response) {
+				console.log("initial lookup", hostname, response.data);
+				vm.choices = response.data;
+				if (vm.choices.length === 1) {
+					vm.data.host = vm.choices[0];
+				}
+			});
+		}
+		
+		User.onchange(function() {
+			vm.superuser = User.isSuperuser();
+		});
+	}
+
+	
+
+	this.toogleSuperuser = function() {
+		this.inSuperUserMode = !this.inSuperUserMode;
+	}
+
+	this.searchHosts = function(hostname) {
+
+		if (hostname && hostname.length > 2) {
+			var params = {
+				hostname : hostname
+			};
+			return $http.get('http://sera.adeo.no/api/v1/servers', {
+				params : params
+			}).then(function(response) {
+				console.log("search", hostname, response.data);
+				vm.choices = response.data
+			});
+
+		}
+	};
+
+	this.submitOrder = function() {
+		console.log("creating new order", this.data);
+		BastaService.submitOrderWithUrl('rest/vm/operations', this.data);
+	};
 } ];
