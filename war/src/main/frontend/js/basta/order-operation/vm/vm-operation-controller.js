dc4be392574d763070e1db2cf52cfddbc7c8223b
Hans-Petter Vadseth <v137023@nav.no>
2015-12-16 10:29:24 +0100

    AURA-1087 Fritekst variant av nodesøk

--- war/src/main/frontend/js/basta/order-operation/vm/vm-operation-controller.js
+++ war/src/main/frontend/js/basta/order-operation/vm/vm-operation-controller.js
@@ -4,93 +4,95 @@ var angular = require('angular');
 
 module.exports = [ '$routeParams', "$http", 'BastaService', 'User', 'errorService', function($routeParams, $http, BastaService, User, errorService) {
 
-	this.choices = [];
-
+	var Map = require("collections/map");
+	this.results = new Map(); 
+	
 	this.data = {
-		hostname : undefined,
-		host : undefined,
+		hostname : $routeParams.hostname,
+		
 	}
-	this.inSuperUserMode = false;
-	this.hasAccess=false;
+	this.hasAccess = true;
 
 	var vm = this;
 
 	init($routeParams.hostname);
 
 	// Initial setup
-	function init(hostname) {
-		if (hostname) {
-			vm.data.hostname = hostname;
-			return lookupSera(hostname, function(response) {
-//				console.log("initial lookup", hostname, response.data);
-				vm.choices = _.map(response.data, function(host) {
-					return host.hostname
-				});
-				if (response.data.length === 1) {
-					setSelected(response.data[0]);
-				}
-			});
+	function init() {
+		if (vm.data.hostname) {
+			console.log("initial lookup ", vm.data.hostname);
+			return findHostnames(vm.data.hostname);
 		}
-		
+
 		User.onchange(function() {
 			vm.superuser = User.isSuperuser();
 		});
 	}
 
-	function setSelected(host){
-		vm.data.host = host;
-		vm.hasAccess = User.hasEnvironmentClassAccess(host.environmentClass);
+	function findHostnames(hostnames) {
+		if (!hostnames){
+			return;
+		}
+
+		var hostnameArray = hostnames.split(",").map(function(s){
+			return s.trim();
+		});
+		// remove not existing in searchstring
+		vm.results.keys().forEach(function(key){
+			if(hostnameArray.indexOf(key) === -1){
+				console.log("key is not in searchstring", key);
+				vm.results.delete(key);
+			}
+		});
+		
+		// add new elements
+		hostnameArray.forEach(function(hostname) {
+//			console.log("new host", hostname);
+			if (!vm.results.has(hostname) && hostname.length > 3) {
+				lookupSera(hostname);
+			}
+		});
 	}
-	
-	function lookupSera(hostname, onSuccess) {
+
+	function lookupSera(hostname) {
 		var params = {
 			hostname : hostname
 		};
 		return $http.get('http://sera.adeo.no/api/v1/servers', {
 			params : params
-		}).then(onSuccess, errorService.handleHttpError('Sera lookup'));
-	}
-
-
-	this.toogleSuperuser = function() {
-		this.inSuperUserMode = !this.inSuperUserMode;
-	}
-
-	this.onSelectHost = function() {
-		return lookupSera(vm.data.hostname,function(response) {
-//			console.log("selected", vm.data.hostname, response.data);
+		}).then(function(response) {
+//			console.log("lookup", vm.data.hostname, response.data);
 			if (response.data.length === 1) {
-				setSelected(response.data[0]);
+				vm.results.set(hostname, response.data[0]);
 			}
-		});
+		}, errorService.handleHttpError('Sera lookup'));
 	}
 
-	this.searchHosts = function(hostname) {
-		if (hostname && hostname.length > 3) {
-		return lookupSera(hostname, function(response) {
-//				 console.log("search", hostname, response.data);
-				vm.choices = _.map(response.data, function(host) {
-					return host.hostname
-				})
-			});
+	this.hostnameChanged = function() {
+		return findHostnames(vm.data.hostname);
+	}
+	
+	function extractHostnames(){
+		return vm.results.map(function(host){
+			return host.hostname
+		})
+	}
 
-		}
-	};
 
 	this.start = function() {
-		var hostnames = [vm.data.host.hostname];
+		var hostnames = extractHostnames();
 		console.log("starting", hostnames);
 		BastaService.createOrderNoFlatmap('rest/vm/operations/start', hostnames);
 	};
 
 	this.stop = function() {
-		var hostnames = [vm.data.host.hostname];
+		var hostnames = extractHostnames();
 		console.log("stopping", hostnames);
 		BastaService.createOrderNoFlatmap('rest/vm/operations/stop', hostnames);
 	};
 
 	this.remove = function() {
-		var hostnames = [vm.data.host.hostname];
+		var hostnames = extractHostnames();
 		console.log("deleting", hostnames);
 		BastaService.createOrderNoFlatmap('rest/vm/operations/decommission', hostnames);
 	};
