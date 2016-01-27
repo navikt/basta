0d540fd92bde245e722ed2457322ccab788f2190
Hans-Petter Vadseth <v137023@nav.no>
2016-01-27 08:38:57 +0100

    Bytter navn på test go gjør klar til merging inn i master

--- war/gulp-tasks/gulp-tests.js
+++ war/gulp-tasks/gulp-tests.js
@@ -5,49 +5,18 @@ var gutil = require('gulp-util');
 
 var child;
 
-gulp.task("start:server", function(){
-    child = spawn("mvn", ["exec:java", "-Dexec.mainClass=no.nav.aura.basta.StandaloneBastaJettyRunner","-Dexec.classpathScope=test"], {cwd: process.cwd()}),
-        stdout = '',
-        stderr = '';
-	
-//	child = spawn("ls", ["-la"], {cwd: process.cwd()}),
-//  stdout = '',
-//  stderr = '';
 
-    child.stdout.setEncoding('utf8');
-
-    child.stdout.on('data', function (data) {
-        stdout += data;
-        gutil.log(data);
-    });
-
-    child.stderr.setEncoding('utf8');
-    child.stderr.on('data', function (data) {
-        stderr += data;
-        gutil.log(gutil.colors.red(data));
-        gutil.beep();
-    });
-
-    child.on('close', function(code) {
-        gutil.log("Done with exit code", code);
-        gutil.log("You access complete stdout and stderr from here"); // stdout, stderr
-    });
-
-	
-});
 
 gulp.task('runProtractor', function() {
 	return gulp.src([ "./src/tests/js/*.js" ]).pipe(protractor({
-		configFile : "./src/test/js/protractor.js",
+		configFile : "./src/test/js/protractor_standalone_test.js",
 		args : [ '--baseUrl', 'http://localhost:1337' ]
 	})).on('error', function(e) {
 		throw e
 	})
-	
-	
 });
 
-gulp.task("test",['runProtractor'], function() {
+gulp.task("e2e-test",['runProtractor'], function() {
 	
 
 })
