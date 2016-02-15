fb13ebdf01d99f00b160a857b671985458f94071
Hans-Petter Vadseth <v137023@nav.no>
2016-02-15 13:27:37 +0100

    Kommenterer ut channel

--- war/src/main/frontend/js/basta/order-menu/order-menu-controller.js
+++ war/src/main/frontend/js/basta/order-menu/order-menu-controller.js
@@ -32,7 +32,7 @@ module.exports = ['$location','User', function ($location, User) {
         menuItem("Database", 'Oracle', 'oracle.png', '/oracle_order'),
         menuItem("Windows",'Server','windows.png','/windows_order'),
         menuItem("MQ",'Queue','websphere.png','/mq_queue_order'),
-        menuItem("MQ",'Channel','websphere.png','/mq_channel_order'),
+//        menuItem("MQ",'Channel','websphere.png','/mq_channel_order'),
         
     ];
 
