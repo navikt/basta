d45d8ec345b04ea0cbec03c7e398d49e246d21b3
Horvi, Johnny <Johnny.Horvi@nav.no>
2016-02-24 13:58:01 +0100

    fixed errorhandling for db-order

--- war/src/main/frontend/js/basta/controllers/error_controller.js
+++ war/src/main/frontend/js/basta/controllers/error_controller.js
@@ -1,73 +1,73 @@
 'use strict';
 
 var util = require('../utils/util');
-module.exports = ['$rootScope','$scope', function($rootScope, $scope) {
+module.exports = ['$rootScope', '$scope', function ($rootScope, $scope) {
 
     function getField(object, fields) {
-      if (object === null || fields.length === 0) 
-        return object;
-      else {
-        var name = fields[0];
-        fields.shift();
-        return getField(object[name], fields);
-      }
+        if (object === null || fields.length === 0)
+            return object;
+        else {
+            var name = fields[0];
+            fields.shift();
+            return getField(object[name], fields);
+        }
     }
 
-    $scope.isObjectEmpty = function(obj) {
-      for(var prop in obj) {
-        if(obj.hasOwnProperty(prop))
-            return false;
-      }
-      return true;
+    $scope.isObjectEmpty = function (obj) {
+        for (var prop in obj) {
+            if (obj.hasOwnProperty(prop))
+                return false;
+        }
+        return true;
     };
-    
+
     $scope.errors = {};
 
-    $scope.$on('resetAllErrors', function(){
+    $scope.$on('resetAllErrors', function () {
         console.log('resetting error');
         $scope.errors = {};
     });
 
 
-    $rootScope.$on("$locationChangeStart",function(event, next, current){
-       $scope.errors = {};
+    $rootScope.$on("$locationChangeStart", function (event, next, current) {
+        $scope.errors = {};
     });
 
 
-    $scope.$on('GeneralError', function(msg, error) {
-//	console.log("Error", msg, error)
-      if (error.removeName) {
-        if ($scope.errors[error.removeName]) {
-          delete $scope.errors[error.removeName];
+    $scope.$on('GeneralError', function (msg, error) {
+        //console.log("Error", msg, error)
+        if (error.removeName) {
+            if ($scope.errors[error.removeName]) {
+                delete $scope.errors[error.removeName];
+            }
+        } else {
+            var message = "";
+            var description = "";
+            if (error.httpError) {
+                // http error arguments: data, status, headers, config
+
+                message += 'Feil oppstått! Http-kode ' + error.httpError.status;
+
+                var data = error.httpError.data;
+                //HTML
+                if (_.isString(data) && data.indexOf('<') === 0) {      //TODO proper check for HTML
+                    data = util.xmlToJsonRaw(data);
+                    var detailedMessage = getField(data, ['html', 'head', 'title']);
+                    if (detailedMessage)
+                        description += detailedMessage;
+                    else
+                        description += ' ';
+                } else if (_.isString(data) && error.httpError.headers('content-type') === 'text/plain') {
+                    description += data;
+                } else if (_.isEmpty(data) && error.httpError.status === 401) {
+                    message += ' (Not authorized)';
+                }
+            }
+            if (data.message)
+                message += ' ' + data.message + ' ';
+
+            $scope.errors[error.name] = {message: message, description: description};
         }
-      } else {
-        var message = "";
-        var description="";
-        if (error.httpError) {
-          // http error arguments: data, status, headers, config
-
-          message += 'Feil oppstått! Http-kode ' + error.httpError.status;
-
-          var data = error.httpError.data;
-            //HTML
-          if (_.isString(data) && data.indexOf('<') === 0) {      //TODO proper check for HTML
-            data = util.xmlToJsonRaw(data);
-            var detailedMessage = getField(data, ['html', 'head', 'title']);
-            if (detailedMessage)
-                description += detailedMessage;
-            else
-              description += ' ';
-          } else if (_.isString(data) && error.httpError.headers('content-type') === 'text/plain'){
-              description += data;
-          } else if (_.isEmpty(data) && error.httpError.status === 401){
-              message += ' (Not authorized)';
-          }
-        }
-        if (error.message)
-          message += error.message + ' ';
-
-        $scope.errors[error.name] = {message : message , description: description};
-      }
     });
-  
-  }];
+
+}];
--- war/src/main/frontend/js/basta/services/notification_service.js
+++ war/src/main/frontend/js/basta/services/notification_service.js
@@ -1,7 +1,6 @@
 'use strict';
 
 module.exports = ['$resource', function($resource) {
-
         return $resource('rest/system/notifications/active', {}, {
            query: {method:'GET',params:{},isArray:true}
         });
