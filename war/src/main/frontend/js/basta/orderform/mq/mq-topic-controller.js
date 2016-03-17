f7ac65035d455ad3ed690aa8117f4f97b3a0aed0
Hans-Petter Vadseth <v137023@nav.no>
2016-03-17 13:56:28 +0100

    typeahead som git ett og ett nivå av topicstring

--- war/src/main/frontend/js/basta/orderform/mq/mq-topic-controller.js
+++ war/src/main/frontend/js/basta/orderform/mq/mq-topic-controller.js
@@ -1,31 +1,31 @@
 'use strict';
 
-module.exports = [ '$http', 'errorService', 'BastaService',  "$rootScope",'$routeParams', '$filter',
-		function($http, errorService,  BastaService, $rootScope, $routeParams, $filter) {
+module.exports = [ '$http', 'errorService', 'BastaService', "$rootScope", '$routeParams', '$filter',
+		function($http, errorService, BastaService, $rootScope, $routeParams, $filter) {
 
 			this.data = {
-				environmentClass :  $routeParams.environmentClass || 'u',
-				environmentName :  $routeParams.environmentName,
-				application :  $routeParams.application,
+				environmentClass : $routeParams.environmentClass || 'u',
+				environmentName : $routeParams.environmentName,
+				application : $routeParams.application,
 				queueName : $routeParams.queueName,
 				queueManager : undefined,
 				fasitAlias : undefined,
-				topicString:undefined,
-				topicName:undefined,
+				topicString : "",
+				topicName : undefined,
 			}
 			this.creates = [];
-			this.topics=['1', '2'];
+			this.topics = [ '1', '2' ];
 			this.validation = {};
 			this.inEditQueueNameMode = false;
 			var ctrl = this;
-			
-			function init(){
+
+			function init() {
 				generateFasitAlias();
 				resetValidation();
 				generateTopicString();
 				updateQueueMananger();
 			}
-			
+
 			this.changeApplication = function() {
 				generateFasitAlias();
 				resetValidation();
@@ -46,7 +46,7 @@ module.exports = [ '$http', 'errorService', 'BastaService',  "$rootScope",'$rout
 				generateTopicString();
 				updateQueueMananger();
 			}
-			
+
 			this.changeTopicString = function() {
 				generateFasitAlias();
 				resetValidation();
@@ -63,15 +63,24 @@ module.exports = [ '$http', 'errorService', 'BastaService',  "$rootScope",'$rout
 
 			this.changeQueueManager = function() {
 			}
+			
+			this.changeTopicString = function() {
+				console.log("change topic")
+				generateFasitAlias();
+			}
 
 			function updateQueueMananger() {
 				if (ctrl.data.environmentName && ctrl.data.application) {
 					$rootScope.$broadcast('UpdateQueueManangerEvent', ctrl.data.queueManager);
 				}
 			}
-			
+
 			function generateTopicString() {
-//				ctrl.data.topicString = ctrl.data.environmentName + "/" ;
+				// ctrl.data.topicString = ctrl.data.environmentName + "/" ;
+			}
+			
+			function generateTopicName() {
+				// ctrl.data.topicString = ctrl.data.environmentName + "/" ;
 			}
 
 			function generateFasitAlias() {
@@ -79,42 +88,58 @@ module.exports = [ '$http', 'errorService', 'BastaService',  "$rootScope",'$rout
 					console.log("Will not generate new fasitAlias in editmode");
 					return;
 				}
-				ctrl.data.fasitAlias = ctrl.data.application + "_" ;
+				var alias=ctrl.data.application
+				var topicString = ctrl.data.topicString;
+				if(topicString && topicString.indexOf('/')!==-1){
+					alias= alias + '_' + _.last(topicString.split("/"));
+				}
+				ctrl.data.fasitAlias = alias;
 			}
 			
-			function getTopics(){
-				if(!ctrl.data.queueManager){
+			function getTopics() {
+				if (!ctrl.data.queueManager) {
 					console.log("no queuemanager")
 					return [];
 				}
 				return $http.get("rest/v1/mq/topics", {
 					params : {
-						environmentClass:ctrl.data.environmentClass,
-						queueManager: ctrl.data.queueManager
+						environmentClass : ctrl.data.environmentClass,
+						queueManager : ctrl.data.queueManager
 					},
 					cache : true
-				}).then(function(response){
+				}).then(function(response) {
 					return response.data;
 				});
 			}
-			
-			
-			this.getTopicStrings = function(searchVal){
+
+			function rightTrunc(topicString, searchVal) {
+				var nextSlash = topicString.indexOf("/", searchVal.length);
+				if (nextSlash === -1) {
+					return topicString
+				}
+				return topicString.substring(0, nextSlash);
+
+			}
+
+			this.getTopicStrings = function(searchVal) {
 				return getTopics().then(function(data) {
-					var topics= _.chain(data)
-					.filter(function(topic){
+					var topics = _.chain(data).filter(function(topic) {
 						return topic.topicString.indexOf(searchVal) === 0;
 					})
-					.map(function(topic){
-						return topic.topicString;
+					.map(function(topic) {
+						var trunc = rightTrunc(topic.topicString, searchVal);
+//						console.log("truncing", topic.topicString, trunc);
+						return trunc;
 					})
-					
+					.unique()
+//					.filter(function(value) {
+//						return value.length > 0;
+//					})
 					.value();
 					console.log("topics", topics);
 					return topics;
 				});
 			}
-
 
 			function hasValidationError() {
 				return ctrl.validation.fasit || hasMqValidationError();
@@ -129,7 +154,7 @@ module.exports = [ '$http', 'errorService', 'BastaService',  "$rootScope",'$rout
 					console.log("We have validation errors", ctrl.validation)
 				} else {
 					console.log("Posting mq queue order", ctrl.data)
-					 BastaService.postOrder('rest/v1/mq/order/topic', ctrl.data);
+					BastaService.postOrder('rest/v1/mq/order/topic', ctrl.data);
 				}
 			}
 
@@ -137,7 +162,7 @@ module.exports = [ '$http', 'errorService', 'BastaService',  "$rootScope",'$rout
 				ctrl.sendOrder();
 
 			};
-			
+
 			init();
 
 		} ];
