055c352dec9ca6dbc57728f3320bd4e273decfb8
Horvi, Johnny <Johnny.Horvi@nav.no>
2016-09-26 07:46:00 +0200

    reformat

--- war/src/test/js/bastaserver.js
+++ war/src/test/js/bastaserver.js
@@ -5,43 +5,42 @@ var server;
 var logPrefix = "[bastaServer]"
 
 function log(message) {
-	console.log(logPrefix, new Date(), message);
+    console.log(logPrefix, new Date(), message);
 }
 
 function start(port) {
-	var mvn = process.platform === "win32" ? "mvn.cmd" : "mvn";
-	var options = {
-		detached : false
-	}
-	server = spawn(mvn, [ "exec:java", "-Dexec.mainClass=no.nav.aura.basta.StandaloneBastaJettyRunner", "-Dexec.classpathScope=test" , "-Dport="+ port], options);
+    var mvn = process.platform === "win32" ? "mvn.cmd" : "mvn";
+    var options = {
+        detached: false
+    }
+    server = spawn(mvn, ["exec:java", "-Dexec.mainClass=no.nav.aura.basta.StandaloneBastaJettyRunner", "-Dexec.classpathScope=test", "-Dport=" + port], options);
 
-	server.stdout.on('data', function(data) {
-		if (data.indexOf("Jetty started on port" ) > -1) {
-			log("################### Started jetty");
-		}
-		 log('stdout: ' + data);
-	});
+    server.stdout.on('data', function (data) {
+        if (data.indexOf("Jetty started on port") > -1) {
+            log("################### Started jetty");
+        }
+        log('stdout: ' + data);
+    });
 
-	server.stderr.on('data', function(data) {
-		log('stdout: ' + data);
+    server.stderr.on('data', function (data) {
+        log('stdout: ' + data);
 
-	});
+    });
 
-	log("Starting server with pid " + server.pid)
-	var time = 20;
-	var stop = new Date().getTime();
-	while (new Date().getTime() < stop + time * 1000) {
-		;
-	}
-	return server;
+    log("Starting server with pid " + server.pid)
+    var time = 20;
+    var stop = new Date().getTime();
+    while (new Date().getTime() < stop + time * 1000) {
+    }
+    return server;
 }
 
 function stop() {
-	log("Stopping server with pid", server.pid)
-	kill(server.pid);
+    log("Stopping server with pid", server.pid)
+    kill(server.pid);
 }
 
 module.exports = {
-	start : start,
-	stop : stop,
+    start: start,
+    stop: stop,
 }
