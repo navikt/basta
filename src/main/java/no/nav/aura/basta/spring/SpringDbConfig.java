9c2d1af945fc65ceb555ff1ecc77d2de998cd60c
Rokke, Sten Ivar <Sten.Ivar.Rokke@nav.no>
2018-08-23 10:59:36 +0200

    Commented out flyway migration for now

--- src/main/java/no/nav/aura/basta/spring/SpringDbConfig.java
+++ src/main/java/no/nav/aura/basta/spring/SpringDbConfig.java
@@ -11,17 +11,16 @@ import no.nav.aura.basta.RootPackage;
 import oracle.net.ns.SQLnetDef;
 import oracle.ucp.jdbc.PoolDataSource;
 import oracle.ucp.jdbc.PoolDataSourceFactory;
-import org.eclipse.jetty.plus.jndi.Resource;
 import org.flywaydb.core.Flyway;
 import org.hibernate.cache.ehcache.EhCacheRegionFactory;
 import org.hibernate.ejb.HibernatePersistence;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.DependsOn;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.jndi.JndiObjectFactoryBean;
 import org.springframework.orm.hibernate3.HibernateExceptionTranslator;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
@@ -36,8 +35,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 public class SpringDbConfig {
 
     @Bean(name = "entityManagerFactory")
-    @DependsOn("flyway")
-    public EntityManagerFactory getEntityManagerFactory(DataSource dataSource) {
+    public EntityManagerFactory getEntityManagerFactory(@Qualifier("getDataSource") DataSource dataSource) {
         LocalContainerEntityManagerFactoryBean factoryBean = new LocalContainerEntityManagerFactoryBean();
         factoryBean.setDataSource(dataSource);
         Properties jpaProperties = new Properties();
@@ -69,14 +67,50 @@ public class SpringDbConfig {
         return new JpaTransactionManager();
     }
 
+    @Bean
+    public DataSource getDataSource(
+            @Value("${BASTADB_URL}") String dbUrl,
+            @Value("${BASTADB_ONSHOSTS}") String onsHosts,
+            @Value("${BASTADB_USERNAME}") String dbUsername,
+            @Value("${BASTADB_PASSWORD}") String dbPassword) throws
+            SQLException {
+        PoolDataSource poolDataSource = PoolDataSourceFactory.getPoolDataSource();
+        poolDataSource.setURL(dbUrl);
+        poolDataSource.setUser(dbUsername);
+        poolDataSource.setPassword(dbPassword);
+        poolDataSource.setConnectionFactoryClassName(getConnectionFactoryClassName());
+        if(dbUrl.toLowerCase().contains("failover")) {
+            if (onsHosts != null) {
+                poolDataSource.setONSConfiguration("nodes=" + onsHosts);
+            }
+            poolDataSource.setFastConnectionFailoverEnabled(true);
+        }
+        Properties connProperties = new Properties();
+        connProperties.setProperty(SQLnetDef.TCP_CONNTIMEOUT_STR, "3000");
+        connProperties.setProperty("oracle.jdbc.thinForceDNSLoadBalancing", "true");
+        // Optimizing UCP behaviour https://docs.oracle.com/database/121/JJUCP/optimize.htm#JJUCP8143
+        poolDataSource.setInitialPoolSize(1);
+        poolDataSource.setMinPoolSize(1);
+        poolDataSource.setMaxPoolSize(20);
+        poolDataSource.setMaxConnectionReuseTime(300); // 5min
+        poolDataSource.setMaxConnectionReuseCount(100);
+        poolDataSource.setConnectionProperties(connProperties);
+        return poolDataSource;
+    }
 
-    @Bean(initMethod = "migrate")
+/*    @Bean(initMethod = "migrate")
     @DependsOn("getDataSource")
+    @Conditional(value=DevDataSourceCondition.class)
     Flyway flyway(@Qualifier("getDataSource") DataSource datasource) {
         Flyway flyway = new Flyway();
         flyway.setBaselineOnMigrate(true);
         flyway.setLocations("classpath:db/migrations/bastaDB");
         flyway.setDataSource(datasource);
+
         return flyway;
+    }*/
+
+    private String getConnectionFactoryClassName() {
+        return "oracle.jdbc.pool.OracleDataSource";
     }
 }
