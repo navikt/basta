c997653d9f408892baef41cb221c25d45c14b9d8
Mats Byfuglien <mats.byfugien@nav.no>
2018-11-28 15:26:38 +0100

    Reduced timeout in test

--- src/test/js/pages/login_partials.js
+++ src/test/js/pages/login_partials.js
@@ -14,7 +14,7 @@ Login.prototype = {
     currentUser: function () {
         var loginContainer = element(by.id('loginContainer'));
         var userNameLabel = loginContainer.element(by.id('currentUser'));
-        browser.sleep(2000)
+        browser.sleep(1000)
         return userNameLabel.getText()
     },
 
