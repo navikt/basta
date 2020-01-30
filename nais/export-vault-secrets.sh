d2beb9eb98929f6d3c456143cba222ab9750088a
matsbyfl <mats@byfuglien.net>
2020-01-30 16:45:37 +0100

    fixed syntax in script reading secrets from vault

--- nais/export-vault-secrets.sh
+++ nais/export-vault-secrets.sh
@@ -10,25 +10,29 @@ then
 fi
 
 if test -d /var/run/secrets/nais.io/bastaDB-config;
+then
   echo "Setting DB config"
   ls -la /var/run/secrets/nais.io/bastaDB-config
-  export BASTADB_URL=$(cat /var/run/secrets/nais.io/bastaDB-config/url)
-  export BASTADB_ONSHOSTS=$(cat /var/run/secrets/nais.io/bastaDB-config/onsHost)
+  export BASTADB_URL=$(cat /var/run/secrets/nais.io/bastaDB-config/jdbc_url)
+  export BASTADB_ONSHOSTS=$(cat /var/run/secrets/nais.io/bastaDB-config/ons_host)
 fi
 
 if test -d /var/run/secrets/nais.io/srvbasta;
+then
   echo "Setting srvbasta creds"
   export SRVBASTA_USERNAME=$(cat /var/run/secrets/nais.io/srvbasta/username)
   export SRVBASTA_PASSWORD=$(cat /var/run/secrets/nais.io/srvbasta/password)
 fi
 
 if test -d /var/run/secrets/nais.io/user_orchestrator;
+then
   echo "Setting Orchestrator creds"
   export USER_ORCHESTRATOR_USERNAME=$(cat /var/run/secrets/nais.io/user_orchestrator/username)
   export USER_ORCHESTRATOR_PASSWORD=$(cat /var/run/secrets/nais.io/user_orchestrator/password)
 fi
 
 if test -d /var/run/secrets/nais.io/basta_mq_dev
+then
   echo "Setting mq dev creds"
   export BASTA_MQ_U_USERNAME=$(cat /var/run/secrets/nais.io/basta_mq_dev/username)
   export BASTA_MQ_U_PASSWORD=$(cat /var/run/secrets/nais.io/basta_mq_dev/password)
@@ -39,30 +43,35 @@ if test -d /var/run/secrets/nais.io/basta_mq_dev
 fi
 
 if test -d /var/run/secrets/nais.io/basta_mq_prod;
+then
   echo "Setting mq prod creds"
   export BASTA_MQ_P_USERNAME=$(cat /var/run/secrets/nais.io/basta_mq_prod/username)
   export BASTA_MQ_P_PASSWORD=$(cat /var/run/secrets/nais.io/basta_mq_prod/password)
 fi
 
 if test -d /var/run/secrets/nais.io/basta_security_ca_prod;
+then
   echo "Setting security ca prod creds"
   export SECURITY_CA_ADEO_USERNAME=$(cat /var/run/secrets/nais.io/basta_security_ca_prod/username)
   export SECURITY_CA_ADEO_PASSWORD=$(cat /var/run/secrets/nais.io/basta_security_ca_prod/password)
 fi
 
 if test -d /var/run/secrets/nais.io/basta_security_ca_preprod;
+then
   echo "Setting security ca preprod creds"
   export SECURITY_CA_PREPROD_USERNAME=$(cat /var/run/secrets/nais.io/basta_security_ca_preprod/username)
   export SECURITY_CA_PREPROD_PASSWORD=$(cat /var/run/secrets/nais.io/basta_security_ca_preprod/password)
 fi
 
 if test -d /var/run/secrets/nais.io/basta_security_ca_test;
+then
   echo "Setting security ca test creds"
   export SECURITY_CA_TEST_USERNAME=$(cat /var/run/secrets/nais.io/basta_security_ca_test/username)
   export SECURITY_CA_TEST_PASSWORD=$(cat /var/run/secrets/nais.io/basta_security_ca_test/password)
 fi
 
 if test -d  /var/run/secrets/nais.io/basta_oem;
+then
   echo "Setting OEM api creds"
   export OEM_USERNAME=$(cat  /var/run/secrets/nais.io/basta_oem/username)
   export OEM_PASSWORD=$(cat  /var/run/secrets/nais.io/basta_oem/password)
