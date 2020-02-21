a94db5f75c0937f78608164966d02fa52d6d3748
matsbyfl <mats@byfuglien.net>
2020-02-21 23:24:53 +0100

    Enable spring cache annotations

--- src/main/java/no/nav/aura/basta/Application.java
+++ src/main/java/no/nav/aura/basta/Application.java
@@ -5,12 +5,14 @@ import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.WebApplicationContext;
 
 import javax.servlet.ServletContext;
 
 @SpringBootApplication
+@EnableCaching
 public class Application extends SpringBootServletInitializer implements WebApplicationInitializer {
 
     public static void main(String[] args) throws Exception {
