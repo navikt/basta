cb349ae9ca4ab7533ffc009f9cf19389a765fe7e
Trong Huu Nguyen <trong.huu.nguyen@nav.no>
2024-10-14 14:54:35 +0200

    feat: replace vault sidecar with renew task

--- src/main/java/no/nav/aura/basta/spring/VaultConfig.java
+++ src/main/java/no/nav/aura/basta/spring/VaultConfig.java
@@ -3,6 +3,7 @@ package no.nav.aura.basta.spring;
 import com.bettercloud.vault.SslConfig;
 import com.bettercloud.vault.Vault;
 import com.bettercloud.vault.VaultException;
+import com.bettercloud.vault.response.AuthResponse;
 import com.bettercloud.vault.response.LookupResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,6 +12,8 @@ import org.springframework.context.annotation.Configuration;
 
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Timer;
+import java.util.TimerTask;
 
 @Configuration
 public class VaultConfig {
@@ -18,10 +21,13 @@ public class VaultConfig {
 
     private static final String VAULT_TOKEN_PROPERTY = "VAULT_TOKEN";
     private static final String VAULT_TOKEN_PATH_PROPERTY = "VAULT_TOKEN_PATH";
+    private static final int MIN_REFRESH_MARGIN  = 10 * 60 * 1000; // 10 min in ms;
+
+    private final Timer timer = new Timer("VaultScheduler", true);;
 
     @Bean
     public Vault getVaultClient() throws VaultError, VaultException {
-        com.bettercloud.vault.VaultConfig vaultConfig = null;
+        com.bettercloud.vault.VaultConfig vaultConfig;
         try {
             vaultConfig = new com.bettercloud.vault.VaultConfig()
                     .address(System.getenv().getOrDefault("VAULT_ADDR", "https://vault.adeo.no"))
@@ -38,11 +44,10 @@ public class VaultConfig {
         final Vault vault = new Vault(vaultConfig, true, 2);
 
         // Verify that the token is ok
-        LookupResponse lookupSelf = null;
+        LookupResponse lookupSelf;
         try {
             lookupSelf = vault.auth().lookupSelf();
             logger.info("Found a Vault token with TTL " + lookupSelf.getTTL());
-            return vault;
         } catch (VaultException e) {
             if (e.getHttpStatusCode() == 403) {
                 throw new VaultError("The application's vault token seems to be invalid", e);
@@ -50,6 +55,33 @@ public class VaultConfig {
                 throw new VaultError("Could not validate the application's vault token", e);
             }
         }
+
+        if (lookupSelf.isRenewable()) {
+            final class RefreshTokenTask extends TimerTask {
+                @Override
+                public void run() {
+                    try {
+                        logger.info("Refreshing Vault token (old TTL = " + vault.auth().lookupSelf().getTTL() + " seconds)");
+                        AuthResponse response = vault.auth().renewSelf();
+                        logger.info("Refreshed Vault token (new TTL = " + vault.auth().lookupSelf().getTTL() + " seconds)");
+                        timer.schedule(new RefreshTokenTask(), suggestedRefreshInterval(response.getAuthLeaseDuration() * 1000));
+                    } catch (VaultException e) {
+                        logger.error("Could not refresh the Vault token", e);
+
+                        // Lets try refreshing again
+                        logger.warn("Waiting 5 secs before trying to refresh the Vault token");
+                        timer.schedule(new RefreshTokenTask(), 5000);
+                    }
+                }
+            }
+
+            logger.info("Starting a refresh timer on the vault token (TTL = " + lookupSelf.getTTL() + " seconds");
+            timer.schedule(new RefreshTokenTask(), suggestedRefreshInterval(lookupSelf.getTTL() * 1000));
+        } else {
+            logger.warn("Vault token is not renewable");
+        }
+
+        return vault;
     }
 
     private static String getProperty(String propertyName) {
@@ -71,6 +103,16 @@ public class VaultConfig {
             }
         } catch (Exception e) {
             throw new RuntimeException("Could not get a vault token for authentication", e);
+        }
+    }
+
+    // We should refresh tokens from Vault before they expire, so we add a MIN_REFRESH_MARGIN margin.
+    // If the token is valid for less than MIN_REFRESH_MARGIN * 2, we use duration / 2 instead.
+    private static long suggestedRefreshInterval(long duration) {
+        if (duration < MIN_REFRESH_MARGIN * 2) {
+            return duration / 2;
+        } else {
+            return duration - MIN_REFRESH_MARGIN;
         }
     }
 
