223aec74cf992c8bf64fa91309dee352e06aa076
Sten Røkke <sten.ivar.rokke@nav.no>
2025-11-17 13:52:08 +0100

    feat: add win2025 enum for vm creation

--- src/main/java/no/nav/aura/basta/backend/vmware/orchestrator/OSType.java
+++ src/main/java/no/nav/aura/basta/backend/vmware/orchestrator/OSType.java
@@ -1,5 +1,5 @@
 package no.nav.aura.basta.backend.vmware.orchestrator;
 
 public enum OSType {
-    flatcar, rhel80, rhel90, win2016, win2019
+    flatcar, rhel80, rhel90, win2016, win2019, win2025
 }
